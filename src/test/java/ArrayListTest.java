9d3164e437e6de0bb56a2f9c4db7fb00c8f8603c
Egor Ogdanets <ogdanets2010@yandex.ru>
2019-12-05 16:01:19 +0300

    Fix unit test

--- src/test/java/ArrayListTest.java
+++ src/test/java/ArrayListTest.java
@@ -4,17 +4,17 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ArrayListTest {
-    private static MyArrayList list;
+    //private static MyArrayList list;
 
-    @BeforeClass
-    public static void init(){
-        System.out.println("init MyArrayList");
-        list = new MyArrayList(5);
-    }
+//    @BeforeClass
+//    public static void init(){
+//        System.out.println("init MyArrayList");
+//        list = new MyArrayList(5);
+//    }
 
     @Test
     public void testAdd(){
-
+        MyArrayList list = new MyArrayList(5);
         for (int i = 1; i <= 6; i++) {
             list.add(Integer.toString(i));
         }
@@ -25,11 +25,29 @@ public class ArrayListTest {
 
     @Test
     public void testAddWithIndex(){
+        MyArrayList list = new MyArrayList(5);
+        for (int i = 1; i <= 6; i++) {
+            list.add(Integer.toString(i));
+        }
 
         list.add(2,"100");
 
         Assert.assertArrayEquals(new Object[]
                         {"1","2","100","3","4","5","6",null},
+                list.getElementData());
+    }
+
+
+    @Test
+    public void testRemove(){
+        MyArrayList list = new MyArrayList(5);
+        for (int i = 1; i <= 6; i++) {
+            list.add(Integer.toString(i));
+        }
+
+        list.remove(4);
+        Assert.assertArrayEquals(new Object[]
+                        {"1","2","3","4","6",null,null,null},
                 list.getElementData());
     }
 
